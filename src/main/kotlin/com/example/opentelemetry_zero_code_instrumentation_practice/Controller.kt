27264cbb1b1b7965b10f343359a1330a91fd14d3
Msksgm <msksgm3124@gmail.com>
2025-09-26 15:44:27 +0900

    chore: Span を追加（@WithSpan アノテーションの方が早いらしい）
    https://opentelemetry.io/ja/docs/zero-code/java/spring-boot-starter/api/#span

--- src/main/kotlin/com/example/opentelemetry_zero_code_instrumentation_practice/Controller.kt
+++ src/main/kotlin/com/example/opentelemetry_zero_code_instrumentation_practice/Controller.kt
@@ -1,12 +1,23 @@
 package com.example.opentelemetry_zero_code_instrumentation_practice
 
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.trace.Tracer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class Controller {
+class Controller(openTelemetry: OpenTelemetry) {
+    private val tracer: Tracer = openTelemetry.getTracer("application")
+
     @GetMapping("/ping")
     fun ping(): String {
+        this.pingSpan()
         return "pong"
+    }
+
+    fun pingSpan() {
+        val span = tracer.spanBuilder("pingSpan").startSpan()
+        span.end()
+        return
     }
 }
